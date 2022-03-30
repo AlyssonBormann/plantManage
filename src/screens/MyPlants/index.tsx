1aee0a821f840beff7c048bb992997622717e067
Alysson Bormann <alysson.bormann@gmail.com>
2022-03-30 09:54:10 -0300

    feat: created styles complet MyPlants

---
+++ src/screens/MyPlants/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Header } from "../../components/Header";
+
+import {
+  Container,
+  Spotlight,
+  SpotlighImage,
+  SpotlightText,
+  Plants,
+  PlantsTitle,
+} from "./styles";
+
+export function MyPlants() {
+  return <Container></Container>;
+}
--- src/screens/MyPlants/styles.ts
+++ src/screens/MyPlants/styles.ts
@@ -10,4 +10,37 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.COLORS.background};
 `;
 
-export const Spolight = styled.View``;
+export const Spotlight = styled.View`
+  background-color: ${({ theme }) => theme.COLORS.blue_light};
+  padding-left: 20px;
+  padding-right: 20px;
+  height: 110px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SpotlighImage = styled.Image`
+  width: 60px;
+  height: 60px;
+`;
+
+export const SpotlightText = styled.Text`
+  flex: 1;
+  color: ${({ theme }) => theme.COLORS.blue};
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const Plants = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+
+export const PlantsTitle = styled.Text`
+  font-size: 24px;
+  font-family: ${({ theme }) => theme.COLORS.heading};
+  color: ${({ theme }) => theme.COLORS.heading};
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
